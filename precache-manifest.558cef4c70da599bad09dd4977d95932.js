cd180a6a9375777f699ad845a1e067c4e77c7d97
Nicolas Turner <nicolasrturner@gmail.com>
2018-12-19 11:08:00 -0800

    Updates

---
+++ precache-manifest.558cef4c70da599bad09dd4977d95932.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f3e3d898afa96ce085a",
+    "url": "/nicolasrturner/static/js/runtime~main.1f3e3d89.js"
+  },
+  {
+    "revision": "6cfacb7cfcc040bcc03f",
+    "url": "/nicolasrturner/static/js/main.6cfacb7c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/nicolasrturner/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "6cfacb7cfcc040bcc03f",
+    "url": "/nicolasrturner/static/css/main.6de6100f.chunk.css"
+  },
+  {
+    "revision": "2bbb5956d29e061e3c280aaa7e3418dc",
+    "url": "/nicolasrturner/index.html"
+  }
+];
