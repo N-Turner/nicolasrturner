6504d4deb9ce65226712b509cad1b43ee311828c
Nicolas Turner <nicolasrturner@gmail.com>
2018-12-18 22:27:16 -0800

    Added react app components

---
+++ src/resumeData.js
@@ -0,0 +1,72 @@
+let resumeData = {
+    "imagebaseurl":"https://i.ibb.co/j3g4VGZ/timothy-eberly-531518-unsplash.jpg",
+    "name": "Nicolas Turner",
+    "role": "Software Engineer",
+    "linkedinId":"nicolasrturner@gmail.com",
+    "skypeid": "@nicolasrturner",
+    "roleDescription": "Software Engineer",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nicolasrturner/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/N-Turner",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/nicolasrturner",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a software engineer with solid programming skills and experience developing full-stack applications. I love being part of an industry that pushes society forward and impacts millions of people through the things we build. We live in one of the most exciting times in human history.",
+    "address":"Seattle, WA",
+    "website":"nicolasrturner@gmail.com",
+    "skillsDescription":"Languages and Technologies",
+    "skills":[
+      {
+        "skillname":"FRONT-END: JavaScript, HTML5, CSS3, React, AngularJS, Backbone"
+      },
+      {
+        "skillname":"BACK-END: Node.js, Express, Python, Flask, MongoDB, Mongoose, MySQL, Postgres"
+      },
+      {
+        "skillname":"TESTING/DEPLOYMENT: Docker, AWS, Jest, Git, Cypress, Mocha, Chai, Heroku"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Local Alert Network (LAN)",
+        "description":"Create and share critical information with those around you during a disaster",
+        "imgurl":"images/portfolio/LAN.png",
+        "code":"https://github.com/N3-Hercules"
+        
+      },
+      {
+        "name":"Backpacker",
+        "description":"An outdoor app to assist the modern hiker with planning and tracking their hikes",
+        "imgurl":"images/portfolio/backpacker.jpg",
+        "code":"https://github.com/N3-Hercules"
+       
+      },
+      {
+        "name":"BarBack",
+        "description":"Bar management app to help patrons order drinks without fighting for attention at the bar",  
+        "imgurl":"images/portfolio/barback.png",
+        "code":"https://github.com/N3-Hercules"
+   
+      },
+      {
+        "name":"CS Chatbot Advisor",
+        "description":"A personal advisor for early career students to learn about CS topics",
+        "imgurl":"images/portfolio/chatbot.png",
+        "code":"https://github.com/N3-Hercules"
+     
+      }
+    ],
+  }
+  
+  export default resumeData
